abb7be50ce7dca98c0f41e545915927315e7973a
studylessshape <a664554724@qq.com>
2024-09-06 17:33:31 +0800

    🎉 init(all): init repo

---
+++ AvaloniaApplication1/Converters/EnumNameToArray.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace AvaloniaApplication1.Converters;
+
+public class EnumNameToArray : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        if (value is not Type)
+        {
+            throw new ArgumentException("Value muse be Enum Type", nameof(value));
+        }
+
+        var type = (Type)value;
+        if (!type.IsEnum)
+        {
+            throw new ArgumentException("Value must be Enum Type", nameof(value));
+        }
+
+        return Enum.GetNames(type);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.ObjectModel;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private int _width;
+
+        [ObservableProperty]
+        private int _height;
+
+        [ObservableProperty]
+        private Stretch _stretch;
+
+        public ObservableCollection<Stretch> EnumStrechs { get; } = new();
+
+        public MainWindowViewModel()
+        {
+            foreach (var item in Enum.GetValues<Stretch>())
+            {
+                EnumStrechs.Add(item);
+            }
+        }
+    }
+}
